4796b3a222a74272250931aa2273712b3110051f
Aditya Kumar <adityakumar_me20a8_36@dtu.ac.in>
2023-06-03 01:51:14 +0530

    my_app testing done!, works fine

--- Real_estate_website/settings.py
+++ Real_estate_website/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'my_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
