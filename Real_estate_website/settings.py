938e4247e65be4c648e630f85b0463718e61c723
Aditya Kumar <adityakumar_me20a8_36@dtu.ac.in>
2023-06-25 01:07:35 +0530

    up and working !

--- Real_estate_website/settings.py
+++ Real_estate_website/settings.py
@@ -65,6 +65,12 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS: True
 
+CORS_ALLOWED_ORIGINS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-d113.up.railway.app/"
+]
+
 ROOT_URLCONF = 'Real_estate_website.urls'
 
 TEMPLATES = [
